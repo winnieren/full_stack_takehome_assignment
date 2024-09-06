672c763471395c2326b1d6fcbedde0eab4415a3f
Nicolas Machado <nicolas@lume.ai>
2024-09-06 11:23:25 -0400

    assignment boilerplate commit

---
+++ data-review-assignment/src/components/DataReviewTable.tsx
@@ -0,0 +1,18 @@
+// components/DataReview.tsx
+
+import { useEffect, useState } from "react";
+
+export default function DataReviewTable() {
+
+    useEffect(() => {
+    }, []);
+
+
+
+    return (
+        <div style={{ margin: "20px" }}>
+            <h1>Data Review</h1>
+            {/* Candidates will replace this placeholder with table, tooltips, and modals */}
+        </div>
+    );
+}
---
+++ data-review-assignment/src/pages/index.tsx
@@ -0,0 +1,12 @@
+// pages/index.tsx
+
+import DataReviewTable from "../components/DataReviewTable";
+
+
+export default function Home() {
+  return (
+    <div>
+      <DataReviewTable />
+    </div>
+  );
+}
